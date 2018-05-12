02b4318b6f374fa33b585ccf4b6db92143602acf
Suzuno1932 <38193072+Suzuno1932@users.noreply.github.com>
2018-05-12 18:52:26 -0300

    Header Mejorado

--- Web/Administrador/AdminM.Master.designer.cs
+++ Web/Administrador/AdminM.Master.designer.cs
@@ -22,15 +22,6 @@ namespace Web {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// Control panel.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel panel;
-        
-        /// <summary>
         /// Control nombre_usuario.
         /// </summary>
         /// <remarks>
@@ -38,6 +29,15 @@ namespace Web {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Literal nombre_usuario;
+        
+        /// <summary>
+        /// Control panel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panel;
         
         /// <summary>
         /// Control ContentPlaceHolder1.
